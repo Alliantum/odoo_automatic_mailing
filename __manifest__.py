13c0d2ce893d1e26c22280b898553d767fe6aebd
Iago Alonso <iago.alonso@opensolutions.net>
2021-05-18 14:52:01 +0200

    v2.0.0

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
     'website': "https://www.alliantum.com",
     'category': 'Technical Settings',
     'license': 'AGPL-3',
-    'version': '12.0.1.0.0',
+    'version': '12.0.2.0.0',
     'depends': [
         'sale',
         'account',
